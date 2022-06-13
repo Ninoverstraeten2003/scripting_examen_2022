e57847bff086f80402e5caeaf7a5da01a4ffdc4b
Ninoverstraeten2003 <r0898606@ucll.be>
2022-06-13 18:58:45 +0200

    proberen wordle op complexe manier

--- wordle/solution.py
+++ wordle/solution.py
@@ -1,5 +1,5 @@
 # Write your solution in this file
-
+from itertools import chain 
 with open('words.txt','r') as words:
     with open('output.txt', 'w') as output:
         words_set = set()
@@ -10,26 +10,47 @@ with open('words.txt','r') as words:
     
         secret_word = str('01234567')
         dictt = {
-            'a':[1-7],
-            'b':[0,2-7],
-            'c':[0-1,3-7],
-            'd':[0-2,4-7],
-            'e':[0-3,5-7],
-            'x':[4]
+            'a':chain(range(1,2),range(2,8)),
+            'b':chain(range(0,1),range(2,8)),
+            'c':chain(range(0,2),range(3,8)),
+            'd':chain(range(0,3),range(4,8)),
+            'x':chain(range(0,5),range(6,8))
         }
+        
         no = ['e','f','g','h']
         words = []
-        for i in no:
-            for word in words_set:
-                for letter in word:
-                    if i != letter:
-                        words.append(word)
-        for word in words:
-            
-            for index, letter in tuple(enumerate(word)):
-                if index not in dictt(letter):
+        for word in words_set:
+            status = True
+            for letter in word:
+                if letter in no:
+                    status = False
                     continue
-                output.write(f"{word}\n")
+            if status: 
+                words.append(word)
+        
+        words2 = []
+        for word in words:
+            status = True
+            for index, letter in tuple(enumerate(word)):
+                if letter in ['a','b','c','d','x']:
+                    if index not in dictt[letter]:
+                        status = False 
+                        continue
+            if status:
+                words2.append(word)
+        
+        for w in words2:
+            status = True
+            for i in ['a','b','c','d','e','x']:
+               
+                if w.count(i) != 1:
+                    status = False
+                    continue
+            if status:
+                output.write(f"{w}\n")
+                    
+                    
+                
             
             
             
